62e10c9d16b11aaa57921e4695c401726759822d
smiodek <smiodek@172.23.129.49>
2015-01-25 20:39:12 +0100

    poprawki

--- Pjatk.Tin.FitApp.Api/Properties/AssemblyInfo.cs
+++ Pjatk.Tin.FitApp.Api/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pjatk.Tin.FitApp.Api")]
+[assembly: AssemblyTitle("Pjatk.Tin.FitApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pjatk.Tin.FitApp.Api")]
+[assembly: AssemblyProduct("Pjatk.Tin.FitApp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43dc9783-8188-45d9-a294-5b4a54d36195")]
+[assembly: Guid("a2860d80-0bcb-4e22-b572-dc6c6d403301")]
 
 // Version information for an assembly consists of the following four values:
 //
